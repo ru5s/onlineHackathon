2c4a67e1097dafab6eba31b7da0d86b6c53be5a0
ru5s <hellperock@gmail.com>
2022-12-07 00:31:01 +0500

    Initial Commit

---
+++ OnlineHackathon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OnlineHackathon
+//
+//  Created by Ruslan Ismailov on 07/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
